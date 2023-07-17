1177996c983c5872de5b805a2eb217d110833c47
deelaka99 <123wejith@gmail.com>
2023-07-17 19:23:08 +0530

    services is completed

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./services.css";
+import HeartEmoji from "../../assets/img/heartemoji.png";
+import Glasses from "../../assets/img/glasses.png";
+import Humble from "../../assets/img/humble.png";
+import Card from "../card/Card";
+
+const Services = () => {
+  return (
+    <div className="services">
+      {/*left side */}
+      <div className="awesome">
+        <span>My Awesome</span>
+        <span>services</span>
+        <span>
+          Lorem jnnjdf hffb hdfbhd fd ddd s ds
+          <br />
+          sd bdsbds dbhsbs
+        </span>
+        <button className="button s-button">Download CV</button>
+        <div className="blur s-blur" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/*right side */}
+      <div className="cards">
+        {/**1st card */}
+        <div style={{ left: "14rem" }}>
+          <Card
+            emoji={HeartEmoji}
+            heading={"Design"}
+            detail={"Figma, Adobe Photoshop, Adobe illustrator, Adobe XD"}
+          />
+        </div>
+        {/*2nd card */}
+        <div style={{ top: "12rem", left: "-4rem" }}>
+          <Card
+            emoji={Glasses}
+            heading={"Developer"}
+            detail={"HTML, CSS, JavaScript, React"}
+          />
+        </div>
+        {/*3rd card */}
+        <div style={{ top: "19rem", left: "12rem" }}>
+          <Card
+            emoji={Humble}
+            heading={"UI/UX"}
+            detail={"Figma, Adobe XD"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
